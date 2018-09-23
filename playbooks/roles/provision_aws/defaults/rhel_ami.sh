37a77f04cf7fbe3e48c9545e377f858898911046
Ken Moini <ken@kenmoini.com>
2018-09-23 11:17:49 -0500

    Update rhel_ami.sh

--- playbooks/roles/provision_aws/defaults/rhel_ami.sh
+++ playbooks/roles/provision_aws/defaults/rhel_ami.sh
@@ -11,5 +11,5 @@ do
   access_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Access*" --region ${ec2_region} --output text | sort -r))
   echo "    cloud_access: ${access_images[0]}"
   hourly_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Hourly*" --region ${ec2_region} --output text | sort -r))
-  echo "    hourly: ami-b63769a1"
+  echo "    hourly: ami-6871a115"
 done
